fddb948c82a21f9ab7c6d16bc22f06c8515b4aa6
Hannah Borel <hannahborel>
2022-03-03 14:16:11 -0600

    Portfoio section setup complete.

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,7 @@ import {Person,Mail} from "@material-ui/icons"
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-      <div class={"topbar " + (menuOpen && "active")}>
+      <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                   <div className="left">
                         <a href="#intro" className="logo">HB.</a>

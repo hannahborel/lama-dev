d66977354fc9f3d86f694bb33736794042a8ef6e
Hannah Borel <hannahborel>
2022-03-03 10:11:18 -0600

    Intro section complete

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,36 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
 export default function Intro() {
-  return (
-      <div class="intro" id="intro">
-       
-      </div>
-  )
+  
+      const textRef = useRef()
+       useEffect( () => {
+            init(textRef.current, {
+                   showCursor: true, 
+                   backDelay: 1500,
+                   backspeed: 60,
+                   strings: [' Developer', ' Desinger', ' Creator' ],
+                   });
+             }, [] )
+      return (
+            <div className="intro" id="intro">
+                  <div className="left">
+                        <div className="imgContainer">
+                              <img src="assets/portrait.png" alt="portrait"/>
+                        </div>
+                  </div>
+                  <div className="right">
+                        <div className="wrapper">
+                              <h2> Hi There, I'm</h2>
+                              <h1>Hannah Borel</h1>
+                              <h3>Freelance<span ref={textRef}></span></h3>
+                        </div>
+                        <a href="#portfolio">
+                              <img src="assets/down.png" alt="down arrow" />
+                        </a>
+                  </div>
+            
+            </div>
+      )
 }
